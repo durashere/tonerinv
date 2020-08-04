16403356eca489467f38861bfcc724339878ad8b
durashere <walendes@gmail.com>
2020-08-04 18:54:45 +0200

    build added

---
+++ build/precache-manifest.6cb4cf73998fd724e1c0974eefa7c890.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e0749f098bdc18df313919c8f5b619",
+    "url": "/index.html"
+  },
+  {
+    "revision": "751338a4c82cf0eb46f2",
+    "url": "/static/js/2.9d1d881e.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.9d1d881e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3301e1affef3f0dd2c9",
+    "url": "/static/js/main.ceedb77a.chunk.js"
+  },
+  {
+    "revision": "2c70a81ee48d670bd15a",
+    "url": "/static/js/runtime-main.214b8753.js"
+  }
+]);
